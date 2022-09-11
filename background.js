91b6677ba5a45a5bab5b52d66bca5186c91e35cc
eyal282 <39803639+eyal282@users.noreply.github.com>
2022-09-12 02:27:57 +0300

    Add files via upload

--- background.js
+++ background.js
@@ -1,8 +1,6 @@
-
-setTimeout(function () {
+setInterval(function () {
 	chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-		//console.log(tabs[0].url.search("duelingbook.com"))
-		if(tabs[0].url.search("duelingbook.com") >= 0)
+		if(tabs[0].url && tabs[0].url.search("duelingbook.com") >= 0)
 		{
 			if(tabs[0].id != undefined)
 			{
@@ -15,4 +13,47 @@ setTimeout(function () {
 			}
 		}
 	}); 
-}, 5000);
+}, 2500);
+
+
+
+
+let lifeline;
+
+keepAlive();
+
+chrome.runtime.onConnect.addListener(port => {
+  if (port.name === 'keepAlive') {
+    lifeline = port;
+    setTimeout(keepAliveForced, 295e3); // 5 minutes minus 5 seconds
+    port.onDisconnect.addListener(keepAliveForced);
+  }
+});
+
+function keepAliveForced() {
+  lifeline?.disconnect();
+  lifeline = null;
+  keepAlive();
+}
+
+async function keepAlive() {
+  if (lifeline) return;
+  for (const tab of await chrome.tabs.query({ url: '*://*/*' })) {
+    try {
+      await chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: () => chrome.runtime.connect({ name: 'keepAlive' }),
+        // `function` will become `func` in Chrome 93+
+      });
+      chrome.tabs.onUpdated.removeListener(retryOnTabUpdate);
+      return;
+    } catch (e) {}
+  }
+  chrome.tabs.onUpdated.addListener(retryOnTabUpdate);
+}
+
+async function retryOnTabUpdate(tabId, info, tab) {
+  if (info.url && /^(file|https?):/.test(info.url)) {
+    keepAlive();
+  }
+}
